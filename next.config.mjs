e16dd7c5b411934370f9388508817850c02e629b
Dominic Leon Hackett <dominichackett@gmail.com>
2024-06-23 00:55:45 -0400

    Fixed test claims

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+    reactStrictMode: false,
+    webpack: config => {
+      config.externals.push('pino-pretty', 'lokijs', 'encoding');
+      return config;
+    }
+    ,typescript: {
+      
+      ignoreBuildErrors: true,  
+    }
+  };
+  
 export default nextConfig;
